6a3ac38825a531fbef28405e694579aaf8bb171e
Vishal Chauhan <vishal2825m@gmail.com>
2024-09-30 06:24:08 +0530

    Database connection

--- TODO_APP/backend/app.js
+++ TODO_APP/backend/app.js
@@ -1,10 +1,13 @@
 import express from "express";
+import { config } from "dotenv";
+import { connection } from "./Database/connection.js";
 
 const app = express();
+config({ path: "./config/config.env" });
+
+//Database Connection
+connection();
 const port = process.env.PORT || 3000;
-
-
-
 app.listen(port, () => {
   console.log(`Server listening on port ${port}`);
 });
