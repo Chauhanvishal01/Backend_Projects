820dbbafff3aec312952b2b918f0d1aa7d68cde6
Vishal Chauhan <vishal2825m@gmail.com>
2024-09-30 16:13:36 +0530

    Create Authentication System

--- TODO_APP/backend/app.js
+++ TODO_APP/backend/app.js
@@ -2,6 +2,7 @@ import express from "express";
 import { config } from "dotenv";
 import { connection } from "./Database/connection.js";
 import todoRouter from "./routes/todo.routes.js";
+import userRouter from "./routes/user.routes.js";
 
 const app = express();
 config({ path: "./config/config.env" });
@@ -11,6 +12,7 @@ config({ path: "./config/config.env" });
 app.use(express.json());
 //Routes
 app.use("/api/v1/todo", todoRouter);
+app.use("/api/v1/user", userRouter);
 
 //Database Connection
 connection();
