c6770a6c62f8d40cc89e4554da1d2655bc265abf
Vishal Chauhan <vishal2825m@gmail.com>
2024-10-01 06:53:19 +0530

    Cors configuration

--- TODO_APP/backend/app.js
+++ TODO_APP/backend/app.js
@@ -1,5 +1,6 @@
 import express from "express";
 import { config } from "dotenv";
+import cors from "cors";
 import { connection } from "./Database/connection.js";
 import todoRouter from "./routes/todo.routes.js";
 import userRouter from "./routes/user.routes.js";
@@ -7,9 +8,15 @@ import userRouter from "./routes/user.routes.js";
 const app = express();
 config({ path: "./config/config.env" });
 
-
-
 app.use(express.json());
+app.use(
+  cors({
+    origin: process.env.FRONTEND_URL,
+    credentials: true,
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+  })
+);
 //Routes
 app.use("/api/v1/todo", todoRouter);
 app.use("/api/v1/user", userRouter);
