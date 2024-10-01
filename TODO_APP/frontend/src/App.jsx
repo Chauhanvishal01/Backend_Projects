cfbbb61d6c6d37bcab9dd410bbf8400d6febb5ae
Vishal Chauhan <vishal2825m@gmail.com>
2024-10-01 06:14:38 +0530

    Create Route

--- TODO_APP/frontend/src/App.jsx
+++ TODO_APP/frontend/src/App.jsx
@@ -1,9 +1,18 @@
-import React from 'react'
-
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+import Home from "./components/Home";
+import Register from "./components/Register";
+import Login from "./components/Login";
 const App = () => {
   return (
-    <div>App</div>
-  )
-}
+    <>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/login" element={<Login />} />
+      </Routes>
+    </>
+  );
+};
 
-export default App
+export default App;
