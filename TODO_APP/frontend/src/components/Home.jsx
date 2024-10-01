3bf87afa0164fbb4043adf2c24cd7556b5c30c43
Vishal Chauhan <vishal2825m@gmail.com>
2024-10-01 06:54:05 +0530

    Fetch Todo in frontend

--- TODO_APP/frontend/src/components/Home.jsx
+++ TODO_APP/frontend/src/components/Home.jsx
@@ -1,9 +1,36 @@
-import React from 'react'
-
+import React, { useEffect, useState } from "react";
+import axios from "axios";
 const Home = () => {
-  return (
-    <div>Home</div>
-  )
-}
+  const [todos, setTodos] = useState([]);
+  const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(false);
 
-export default Home
+  useEffect(() => {
+    const fetchTodos = async () => {
+      try {
+        setLoading(true);
+        const res = await axios.get(
+          `${import.meta.env.VITE_BACKEND_URL}/api/v1/todo/get`,
+          {
+            withCredentials: true,
+            headers: {
+              "Content-Type": "application/json",
+            },
+          }
+        );
+
+        setTodos(res.data);
+        setError(null);
+      } catch (error) {
+        setError("Failed to fetch todos");
+      } finally {
+        setLoading(false);
+      }
+    };
+    fetchTodos();
+  }, []);
+
+  return <div>Home</div>;
+};
+
+export default Home;
