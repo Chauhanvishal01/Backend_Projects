0652e6235264e07bb30a3560bc17fcdeb065b5cf
Vishal Chauhan <vishal2825m@gmail.com>
2024-10-08 15:17:31 +0530

    Complete Backend

--- TODO_APP/backend/controllers/user.controller.js
+++ TODO_APP/backend/controllers/user.controller.js
@@ -82,7 +82,10 @@ export const login = async (req, res) => {
 };
 export const logout = async (req, res) => {
   try {
-    res.clearCookie("jwt");
+    res.clearCookie("jwt", {
+      path: "/",
+      httpOnly: true,
+    });
     res.status(200).json({ message: "User logged Out Successfully" });
   } catch (error) {
     res.status(500).json({ message: "Internal Server Error" });
