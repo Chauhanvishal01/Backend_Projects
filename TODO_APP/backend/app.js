882ab227e2ce4306185a7ec53cf6204e520354f5
Vishal Chauhan <vishal2825m@gmail.com>
2024-10-08 07:16:00 +0530

    Install Cookie Parser and config

--- TODO_APP/backend/app.js
+++ TODO_APP/backend/app.js
@@ -1,5 +1,6 @@
 import express from "express";
 import { config } from "dotenv";
+import cookieParser from 'cookie-parser'
 import cors from "cors";
 import { connection } from "./Database/connection.js";
 import todoRouter from "./routes/todo.routes.js";
@@ -9,6 +10,7 @@ const app = express();
 config({ path: "./config/config.env" });
 
 app.use(express.json());
+app.use(cookieParser()); 
 app.use(
   cors({
     origin: process.env.FRONTEND_URL,
